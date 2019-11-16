81cf9615f92a1a3d1696137bf3bb3d323023ec66
Miguel <miasvaleriano@gmail.com>
2019-11-16 17:37:12 -0500

    Modal partially finished
    Modal overlay works but still need to fix birth date and exit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-// VARIABLES
-const $gallery = $('.gallery');
+    // VARIABLES
+    const $gallery = $('.gallery');
+    const $body = $('body');
+    var people = 0;
 
-// FETCH INFO
-fetch('https://randomuser.me/api/?results=12') //https://randomuser.me/api/?results=5000
+    // FETCH INFO
+fetch('https://randomuser.me/api/?results=12&nat=ie') //https://randomuser.me/api/?results=5000
     .then(response => response.json())
-    .then(data => generateCard(data.results));
+    .then(data => generateCard(data.results))
+    .then(plHolder => cardListener());
 
-// HELPER FUNCTIONS
-function generateCard(data) {
-    const contact = data.map(item => `
+    // HELPER FUNCTIONS
+    function generateCard(data) {
+        people = data;
+        const contact = data.map(item => `
     <div class="card">
                 <div class="card-img-container">
                     <img class="card-img" src=${item.picture.thumbnail} alt="profile picture">
@@ -20,5 +24,42 @@ function generateCard(data) {
                 </div>
             </div>
 `).join("");
-    $gallery.append(contact);
+        $gallery.append(contact);
+    }
+
+    // MODAL
+
+function cardListener() {
+    const cards = document.getElementsByClassName('card');
+    for (let i = 0; i < 12; i = i + 1) {
+        cards[i].addEventListener('click', function () { generateModal(i) });
+    }
+};
+
+function generateModal(i) {
+    let temp = people[i];
+    const modalHTML = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${temp.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${temp.name.first} ${temp.name.last}</h3>
+                <p class="modal-text">${temp.email}</p>
+                <p class="modal-text cap">${temp.location.city}</p>
+                <hr>
+                <p class="modal-text">${temp.cell}</p>
+                <p class="modal-text">${temp.location.street.number} ${temp.location.street.name}, ${temp.location.state} ${temp.location.postcode}</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+       </div>
+    `;
+    $body.append(modalHTML);
 }
+
+function buttonListener() {
+    $('#modal-close-btn').click(function () { $('.modal-container').hide() });
+}
+
+
+
